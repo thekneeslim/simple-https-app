0967f804fd80a1a965be259ce604435f3e960076
Denise <denise.lim.sh@gmail.com>
2019-12-11 11:02:42 +0800

    Move https application into separate module

---
+++ https/src/main/java/com/experiment/SimpleHttpsApplication.java
@@ -0,0 +1,12 @@
+package com.experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHttpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleHttpsApplication.class, args);
+    }
+}
